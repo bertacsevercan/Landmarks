e452a6d0b96043a4e59794c4bdd683ef2d5586b8
bertacsevercan <bertac.severcan@gmail.com>
2021-10-24 14:17:35 +0300

    İOS App done

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,51 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Bertaç Severcan on 24.10.2021.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
+
+    var body: some View {
+        NavigationView {
+            List {
+                PageView(pages: modelData.features.map {
+                    FeatureCard(landmark: $0)
+                })
+                    .aspectRatio(3 / 2, contentMode: .fit)
+                    .listRowInsets(EdgeInsets())
+
+                ForEach(modelData.categories.keys.sorted(), id: \.self) {
+                    key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+
+            }
+            .listStyle(InsetListStyle())
+            .navigationTitle(/*@START_MENU_TOKEN@*/"Featured"/*@END_MENU_TOKEN@*/)
+            .toolbar{
+                Button(action: {showingProfile.toggle()}) {
+                    Image(systemName: "person.crop.circle")
+                        .accessibilityLabel("User Profile")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
